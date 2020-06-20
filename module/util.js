54641f60cbd1949787fdccb81ae3282a65c25626
Nathachai Thongniran <inid3a@gmail.com>
2020-06-20 21:24:42 +0700

    chore(log): remove console.log

--- module/util.js
+++ module/util.js
@@ -4,10 +4,6 @@
  * @param {Object} res
  */
 function reply (res, { code = 200, data = null, message = '' }) {
-  console.log(code)
-  console.log(data)
-  console.log(message)
-  console.log(res)
   res.status(code)
   res.json({
     code,
