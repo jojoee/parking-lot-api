0cce78a3ec80c168f1b9b81ba53e00fa32fdc3f4
Nathachai Thongniran <inid3a@gmail.com>
2020-06-22 17:22:57 +0700

    chore(constant): add constant file

--- config/constant.js
+++ config/constant.js
@@ -7,5 +7,23 @@ module.exports = {
     UNAUTHORIZED: 401,
     NOT_FOUND: 404,
     INVALID_ARGUMENT: 409
+  },
+  SLOT_STATUS: {
+    OCCUPIED: 1,
+    UNOCCUPIED: 2
+  },
+  SLOT_SIZE: {
+    SMALL: 1,
+    MEDIUM: 2,
+    LARGE: 3
+  },
+  VEHICLE_SIZE: {
+    SMALL: 1,
+    MEDIUM: 2,
+    LARGE: 3
+  },
+  TICKET_STATUS: {
+    OPEN: 1,
+    CLOSE: 2
   }
 }
