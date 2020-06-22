4fd27916d597300d57710cc64965a20acb9eea17
Nathachai Thongniran <inid3a@gmail.com>
2020-06-22 17:26:37 +0700

    feat(orderranksbynearestrank): add orderRanksByNearestRank function

--- module/util.js
+++ module/util.js
@@ -12,6 +12,38 @@ function reply (res, { code = 200, data = null, message = '' }) {
   })
 }
 
+/**
+ * Order parking lot rank with entryRank by nearest rank
+ *
+ * @param {Number} entryRank
+ * @param {Number[]} ranks list of parking lot rank that's sorted by asc
+ * @returns {Number[]}
+ */
+function orderRanksByNearestRank (entryRank, ranks) {
+  let sortedRanks = []
+  if (entryRank === ranks[0]) {
+    sortedRanks = [...ranks]
+  } else if (entryRank === ranks[ranks.length - 1]) {
+    sortedRanks = [...ranks]
+    sortedRanks.reverse()
+  } else {
+    // calculate rank distance based on entry rank
+    const distKey = {}
+    for (let rank of ranks) {
+      const dist = Math.abs(entryRank - rank)
+      distKey[dist] = distKey[dist] ? distKey[dist].concat([rank]) : [rank]
+    }
+
+    // flatten it
+    for (let dist in distKey) {
+      sortedRanks = sortedRanks.concat(distKey[dist])
+    }
+  }
+
+  return sortedRanks
+}
+
 module.exports = {
-  reply
+  reply,
+  orderRanksByNearestRank
 }
--- module/util.spec.js
+++ module/util.spec.js
@@ -1,7 +1,34 @@
 const assert = require('assert')
+const { orderRanksByNearestRank } = require('./util')
 
 describe('mocha', () => {
   it('able to run', () => {
     assert.ok(true)
+  })
+})
+
+describe('orderRanksByNearestRank', () => {
+  it('returns the same cause entryRank is the lowest', () => {
+    assert.deepStrictEqual(orderRanksByNearestRank(1, [1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+  })
+
+  it('returns a reverse cause entryRank is the highest', () => {
+    assert.deepStrictEqual(orderRanksByNearestRank(5, [1, 2, 3, 4, 5]), [5, 4, 3, 2, 1])
+  })
+
+  it('orders by nearest rank', () => {
+    assert.deepStrictEqual(orderRanksByNearestRank(2, [1, 2, 3, 4, 5]), [2, 1, 3, 4, 5])
+    assert.deepStrictEqual(orderRanksByNearestRank(3, [1, 2, 3, 4, 5]), [3, 2, 4, 1, 5])
+    assert.deepStrictEqual(orderRanksByNearestRank(4, [1, 2, 3, 4, 5]), [4, 3, 5, 2, 1])
+  })
+
+  it('orders chipped ranks by nearest rank', () => {
+    assert.deepStrictEqual(orderRanksByNearestRank(2, [1, 2, 4, 5]), [2, 1, 4, 5])
+    assert.deepStrictEqual(orderRanksByNearestRank(3, [1, 3, 4, 5]), [3, 4, 1, 5])
+    assert.deepStrictEqual(orderRanksByNearestRank(3, [1, 2, 3, 5]), [3, 2, 1, 5])
+    assert.deepStrictEqual(orderRanksByNearestRank(4, [1, 2, 4, 5]), [4, 5, 2, 1])
+
+    assert.deepStrictEqual(orderRanksByNearestRank(4, [1, 4, 5]), [4, 5, 1])
+    assert.deepStrictEqual(orderRanksByNearestRank(2, [1, 2, 5]), [2, 1, 5])
   })
 })
