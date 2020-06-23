dcbaaf4de8262f26d270d996a217e91fdae1186e
Nathachai Thongniran <inid3a@gmail.com>
2020-06-23 09:42:41 +0700

    test(sequelize): update Sequelize config for test environment

--- config/config.js
+++ config/config.js
@@ -1,7 +1,16 @@
-require('dotenv').config()
+const configPath = (process.env.NODE_ENV === 'test') ? '.env.test' : '.env'
+require('dotenv').config({ path: configPath })
 
 module.exports = {
   development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  },
+  test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
